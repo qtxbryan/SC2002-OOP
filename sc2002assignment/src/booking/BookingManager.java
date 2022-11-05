b9d07896976a0778fd70dd23b8bf584dcadf11c3
JY <teojiayun93@gmail.com>
2022-11-05 17:49:58 +0800

    test

---
+++ sc2002assignment/src/booking/BookingManager.java
@@ -0,0 +1,42 @@
+package booking; //change to ManagerEntities
+
+import CineplexEntities.Cinema;
+
+
+public class BookingManager {
+    
+    //called when tyring to make booking
+    // AFTER CHOSEN MOVIE AND SHOWTIME
+    public void printBookingPortal(int movieID){
+        boolean exit = false;
+        //show booking menu until exit chosen
+        while(!exit){
+
+            //If no more seat is available
+            if (cinema.getCinemaStatus() == CinemaStatus.SOLD_OUT){
+                System.out.println("Sorry. This showtime is sold out. Please select another showtime.");
+                exit = true;
+                return;
+            }
+
+            //Show seating plan
+            
+        }
+        System.out.println("==========MakeBooking==========");
+
+
+    }
+
+
+
+    //called when booking is confirmed
+    public int makeBooking(){
+        
+        for (int i = 0; i < getSelectedSeats().size(); i++){
+
+        }
+    }
+    private listBookings(int customerID){
+
+    }
+}
