ff670140d7777d1d5704173c4c45eb2ba659b770
lsb7 <SLIM181@e.ntu.edu.sg>
2022-11-06 00:53:52 +0800

    review getter and setters

--- sc2002assignment/src/review/Review.java
+++ sc2002assignment/src/review/Review.java
@@ -1,15 +1,75 @@
 package review;
-import java.time.LocalDate;
+
+import java.time.LocalDateTime;
 
 public class Review {
 
-	private int reviewID; 
+	//attributes
+
+	private int reviewID; // ID of review
 	private String reviewerName; 
 	private String reviewTitle; 
 	private String reviewBody; 
-	private int movieID; 
+	private int movieID; // ID of movie being reviewed
 	private double score; 
-	private LocalDate dateTime;  
+	private LocalDateTime dateTime; //date and time of review  
 	
-	
+    //Getters
+    public int get_reviewID() {
+        return reviewID;
+    }
+
+    public String get_reviewerName() { 
+		return reviewerName; 
+	}
+
+    public String get_reviewTitle() {
+        return reviewTitle;
+    }
+
+    public String get_reviewBody() {
+        return reviewBody;
+    }
+
+    public int getMovieID() {
+    	return movieID;
+    }
+
+    public double get_score() {
+        return score;
+    }
+
+    public LocalDateTime get_dateTime() {
+        return dateTime;
+    }
+    
+    //Setters
+    public void set_reviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
+    public void set_reviewerName(String reviewerName) { 
+		this.reviewerName = reviewerName; 
+	}
+
+    public void set_reviewTitle(String reviewTitle) {
+        this.reviewTitle = reviewTitle;
+    }
+
+    public void set_reviewBody(String reviewBody) {
+        this.reviewBody = reviewBody;
+    }
+
+    public void set_movieID(int movieID) {
+    	this.movieID = movieID;
+    }
+
+    public void set_score(double score) {
+        this.score = score;
+    }
+
+    public void set_dateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
 }
