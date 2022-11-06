ff0b4fb7237d93f55b710409d17c3979d52e2dbe
JY <teojiayun93@gmail.com>
2022-11-06 15:28:47 +0800

    commit

--- sc2002assignment/src/booking/Booking.java
+++ sc2002assignment/src/booking/Booking.java
@@ -1,20 +1,75 @@
 package booking;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import movie.Movie;
 
+/**
+ * Booking made by Customer
+ */
 public class Booking {
 
+	/**
+	 * 
+	 */
 	private int bookingID; 
 	private List<Ticket> tickets; 
 	private int transactionID; 
-	private LocalDate dateTime; 
+	private LocalDateTime dateTime; 
 	private int movieID; 
 	private int hallNo; 
 	private int cineplexID; 
 	
+	//Methods
+	/**
+	 * Display the booking
+	 */
 	private void displayBooking() {
-		
-		
+		System.out.println("------------");
+		System.out.printf("Booking ID: %d\n", getBookingID());
+		//print movie name
+		//System.out.printf("Movie: %s\n", MovieManager.getInstance().getMoviebyID(getMovieID()).getTitle());
+		System.out.printf("Cineplex: %d\n", getCineplexID());
+		System.out.printf("Hall: %d\n", getHallNo());
+
+		//print showtime in specific format
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy, hh.mma");
+		System.out.printf("Showtime: %s\n", getDateTime().format(formatter));
+
+		//print tickets
+		System.out.printf("Seats booked: ");
+		for (int i = 0; i < getTickets().size(); i++){
+			System.out.printf("%Ticket", getTickets().get(i).getSeat());
+			if (i+1 < getTickets().size()){
+				System.out.printf(",");
+			}
+		System.out.println();
+
+		//print transaction id
+		System.out.printf("Transaction ID: %d\n", getTransactionID());
+		System.out.println("------------");
+		}
 	}
+
+
+	//getters
+	public int getBookingID() {return bookingID;}
+	public List<Ticket> getTickets() {return tickets;}
+	public int getTransactionID(){ return transactionID;}
+	public LocalDateTime getDateTime() {return dateTime;}
+	public int getMovieID() {return movieID;}
+	public int getHallNo() {return hallNo;}
+	public int getCineplexID() {return cineplexID;}
+
+	//setters
+	public void setBookingID(int ID) {this.bookingID = ID;}
+	public void setTickets(List<Ticket> tickets) {this.tickets = tickets;}
+	public void setTransactionID(int transactionID) {this.transactionID = transactionID;}
+	public void setDateTime(LocalDateTime datetime) {this.dateTime = dateTime;}
+	public void setMovieID(int movieID) {this.movieID = movieID;}
+	public void setHallNo(int hallNo) {this.hallNo = hallNo;}
+	public void setCineplexID(int cineplexID) {this.cineplexID = cineplexID;}
+
+	
 }
--- sc2002assignment/src/booking/Ticket.java
+++ sc2002assignment/src/booking/Ticket.java
@@ -1,7 +1,31 @@
 package booking;
 
-public class Ticket {
+import CineplexEntities.seat;
 
-	
-	private int ticketID; 
+
+public class Ticket {
+	private int ticketID;
+	private TicketType ticketType;
+	private Seat seat;
+	private double ticketPrice;
+
+
+	//Constructor
+	public Ticket (TicketType ticketType){
+		this.ticketType = ticketType;
+	}
+
+
+
+	//getters
+	public int getTicketID() {return ticketID;}
+	public TicketType getTicketType() {return ticketType;}
+	public Seat getSeat() {return seat;}
+	public double getTicketPrice() {return ticketPrice;}
+
+	//setters
+	public void setTicketID(int ticketID) {this.ticketID = ticketID;}
+	public void setTicketType(TicketType ticketType) {this.ticketType = ticketType;}
+	public void setSeat(Seat seat) {this.seat = seat;}
+	public void setTicketPrice(double ticketPrice) {this.ticketPrice = ticketPrice;}
 }
